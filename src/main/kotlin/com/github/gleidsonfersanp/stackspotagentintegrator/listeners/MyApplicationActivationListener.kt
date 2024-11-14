f4c942bf679a1a34a8619fb2cd3e26cd7b16e195
GitHub Action <action@github.com>
2024-11-14 12:59:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/gleidsonfersanp/stackspotagentintegrator/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/gleidsonfersanp/stackspotagentintegrator/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.gleidsonfersanp.stackspotagentintegrator.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
